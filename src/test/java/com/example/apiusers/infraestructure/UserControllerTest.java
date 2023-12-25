b4ffa73ae4069f2dd497cc9435d633c9afa63580
carlosmoncho <carlosmonchomolla@gmail.com>
2023-12-25 19:44:57 +0100

    tests

--- src/test/java/com/example/apiusers/infraestructure/UserControllerTest.java
+++ src/test/java/com/example/apiusers/infraestructure/UserControllerTest.java
@@ -3,31 +3,33 @@ package com.example.apiusers.infrastructure;
 import com.example.apiusers.domain.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import javax.sql.DataSource;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @SneakyThrows
     @Test
-    void saveUser() {
+    void saveAndRetrieveUser() {
+        // Guarda un usuario
         var user = new User();
         user.setEmail("test");
         ObjectMapper objectMapper = new ObjectMapper();
@@ -35,5 +37,11 @@ public class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(user))
         ).andExpect(status().isCreated());
+
+        // Recupera el usuario recién creado
+        this.mockMvc.perform(get("/api/users/{userId}", 1L))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.email").value(user.getEmail()));
     }
 }
